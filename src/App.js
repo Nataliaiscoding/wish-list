4f517eea20167ba70cd98656db34229bcb6e6601
Nataliaiscoding <119931845+Nataliaiscoding@users.noreply.github.com>
2024-10-08 23:41:30 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import { data } from './data';
 import './App.css';
+import Photo from './Photo';
 
 function App() {
+
+  const [city, setCity] = useState(data);
+  const [showMore, setShowMore] = useState(false);
+
+  const removeCity = (id) => {
+    let newCity = city.filter(item => item.id !== id);
+    setCity(newCity)
+  }
+
+  const showTextClick = (town) => {
+    town.showMore = !town.showMore;
+    setShowMore(!showMore)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='big'>
+      <div className='container'>
+        <h1>Wish list of {city.length} places to visit between November 2024 and January 2025</h1>
+      </div>
+
+      <div className='containerImg'>
+          <Photo />
+      </div>
+
+      {city.map((town => {
+        const {id, city, image, description, timeForVisit, showMore} = town;
+      
+      return (
+        <div key={id} className='containerPool'>
+          <div className='container'>
+            <h2>{id} - {city}</h2>
+          </div>
+
+          <div className='containerImg'>
+            <img src={image} width='300px' alt='City' />
+          </div>
+
+          <div className='container'>
+            <p>
+            {showMore ? description : description.substring(0, 55) + "..."} 
+            <button className='btnMore' onClick={() => showTextClick(town)}>{showMore ? "Show less" : "Show more"}</button>
+            </p>
+          </div>
+
+          <div className='container'>
+            <h3>Visiting time {timeForVisit}</h3>
+          </div>
+
+          <div className='container'>
+            <button onClick={() => removeCity(id)}>Remove</button>
+          </div>
+        </div>
+)
+      }))}
+
+      <div className='container'>
+        <button onClick={() => setCity([])}>delete all</button>
+      </div>
     </div>
   );
 }
-
 export default App;
