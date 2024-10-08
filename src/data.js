d73990a1c7f570c4b3122e4e1de0e8dc27cc2eba
Nataliaiscoding <119931845+Nataliaiscoding@users.noreply.github.com>
2024-10-08 23:43:47 +0200

    Create data.js

---
+++ src/data.js
@@ -0,0 +1,46 @@
+export const data = [
+    {
+        id: 1,
+        city: "Prague",
+        image: "https://images.unsplash.com/photo-1658077255690-cee3844203e9?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", 
+        imageTwo: "https://plus.unsplash.com/premium_photo-1661963067279-2f7bf970c49c?q=80&w=1949&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", 
+        description: "Prague  is the capital and largest city of the Czech Republic and the historical capital of Bohemia. Situated on the Vltava river, Prague is home to about 1.4 million people. The city has a temperate oceanic climate, with relatively warm summers and chilly winters.",
+        timeForVisit: "11.11.2024",
+        showMore: false,
+    },
+    {
+        id: 2,
+        city: "Amsterdam",
+        image: "https://images.unsplash.com/photo-1468436385273-8abca6dfd8d3?q=80&w=2005&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        imageTwo: "https://plus.unsplash.com/premium_photo-1661878122586-2d75a86f3400?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        description: "It is the capital and the principal commercial and financial centre of the Netherlands. To the scores of tourists who visit each year, Amsterdam is known for its historical attractions, for its collections of great art, and for the distinctive colour and flavour of its old sections, which have been so well preserved.",
+        timeForVisit: "15.12.2024",
+        showMore: false,
+    },
+    {
+        id: 3,
+        city: "London",
+        image: "https://plus.unsplash.com/premium_photo-1671809692422-4893b9e09119?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        imageTwo: "https://images.unsplash.com/photo-1506501139174-099022df5260?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        description: "London is the capital city of the United Kingdom. It is the U.K.'s largest metropolis and its economic, transportation, and cultural centre. London is also among the oldest of the world's great cities, with its history spanning nearly two millennia.",
+        timeForVisit: "27.12.2024",
+        showMore: false,
+    },
+    {
+        id: 4,
+        city: "Paris",
+        image: "https://plus.unsplash.com/premium_photo-1661914178431-fc899737a386?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        imageTwo: "https://images.unsplash.com/photo-1499856871958-5b9627545d1a?q=80&w=2020&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        description: "Paris is the capital and most populous city of France. Situated on the Seine River, in the north of the country, it is in the centre of the Île-de-France region, also known as the région parisienne, Paris Region.",
+        timeForVisit: "06.01.2025",
+        showMore: false,
+    },    {
+        id: 5,
+        city: "Rome",
+        image: "https://images.unsplash.com/photo-1552832230-c0197dd311b5?q=80&w=1996&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        imageTwo: "https://images.unsplash.com/photo-1548585742-1df49e753a83?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fHJvbWV8ZW58MHx8MHx8fDA%3D",
+        description: "Rome is the capital of Italy and the most populous and extensive municipality, ranking among the major European capitals by territory size. It stands in the central-western portion of the Italian Peninsula, on the Tiber River within the Lazio region. Its history spans 28 centuries.",
+        timeForVisit: "22.01.2025",
+        showMore: false,
+    }
+] 
